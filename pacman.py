1a09f5849b33c14ef1279051076b3dc3fab28fa7
chelberserker <bersenjow@gmail.com>
2016-02-23 11:20:40 +0300

    Project files

---
+++ pacman.py
@@ -0,0 +1,214 @@
+import sys
+import pygame
+from pygame.locals import *
+from math import floor
+import random
+
+
+def init_window():
+        pygame.init()
+        pygame.display.set_mode((512, 512))
+        pygame.display.set_caption('Packman')
+
+
+def draw_background(scr, img=None):
+        if img:
+                scr.blit(img, (0, 0))
+        else:
+                bg = pygame.Surface(scr.get_size())
+                bg.fill((0, 0, 0))
+                scr.blit(bg, (0, 0))
+
+
+class GameObject(pygame.sprite.Sprite):
+        def __init__(self, img, x, y, tile_size, map_size):
+                pygame.sprite.Sprite.__init__(self)
+                self.image = pygame.image.load(img)
+                self.screen_rect = None
+                self.x = 0
+                self.y = 0
+                self.tick = 0
+                self.tile_size = tile_size
+                self.map_size = map_size
+                self.set_coord(x, y)
+
+        def set_coord(self, x, y):
+                self.x = x
+                self.y = y
+                self.screen_rect = Rect(floor(x) * self.tile_size, floor(y) * self.tile_size, self.tile_size, self.tile_size )
+
+        def game_tick(self):
+                self.tick += 1
+
+        def draw(self, scr):
+                scr.blit(self.image, (self.screen_rect.x, self.screen_rect.y))
+
+
+class Ghost(GameObject):
+        def __init__(self, x, y, tile_size, map_size):
+                GameObject.__init__(self, './resources/ghost.bmp', x, y, tile_size, map_size)
+                self.direction = 0                # 0 - неподвижно, 1 - вправо, 2 = вниз, 3 - влево, 4 - вверх
+                self.velocity = 4.0 / 10.0        # Скорость в клетках / игровой тик
+
+        def game_tick(self):
+                super(Ghost, self).game_tick()
+                if self.tick % 20 == 0 or self.direction == 0: # Каждые 20 тиков случайно выбираем направление движения. Вариант self.direction == 0 соотвествует моменту первого вызова метода game_tick() у обьекта
+                        self.direction = random.randint(1, 4)
+
+                if self.direction == 1:                        # Для каждого направления движения увеличиваем координату до тех пор пока не достгнем стены. Далее случайно меняем напрвление движения
+                        self.x += self.velocity
+                        if self.x >= self.map_size-1:
+                                self.x = self.map_size-1
+                                self.direction = random.randint(1, 4)
+                elif self.direction == 2:
+                        self.y += self.velocity
+                        if self.y >= self.map_size-1:
+                                self.y = self.map_size-1
+                                self.direction = random.randint(1, 4)
+                elif self.direction == 3:
+                        self.x -= self.velocity
+                        if self.x <= 0:
+                                self.x = 0
+                                self.direction = random.randint(1, 4)
+                elif self.direction == 4:
+                        self.y -= self.velocity
+                        if self.y <= 0:
+                                self.y = 0
+                                self.direction = random.randint(1, 4)
+                self.set_coord(self.x, self.y)
+
+
+class Pacman(GameObject):
+    def __init__(self, x, y, tile_size, map_size):
+        GameObject.__init__(self, './resources/timofey.bmp', x, y, tile_size, map_size)
+        self.direction = 0
+        self.velocity = 1
+        self.bonus = 0
+
+    def swallow(self):
+        super(Pacman, self).game_tick()
+        """
+        if self.x >= Wall.x-1:
+            self.velocity = 0
+        if self.y >= Wall.y-1:
+            self.velocity = 0
+        """
+        if self.bonus != 10:
+            for i in range(len(food)):
+                if (self.x == food[i].x)and(self.y == food[i].y):
+                    food[i].x = food[i].y = 30
+                    self.bonus += 1
+                    print(i, food[i].x, self.bonus)
+
+        else:
+            exit(0)
+
+
+    def game_tick(self):
+        super(Pacman, self).game_tick()
+        x1 = self.x
+        y1 = self.y
+        if self.direction == 1:
+            self.x += self.velocity
+            if self.x >= self.map_size-1:
+                self.x = self.map_size-1
+        elif self.direction == 2:
+            self.y += self.velocity
+            if self.y >= self.map_size-1:
+                self.y = self.map_size-1
+        elif self.direction == 3:
+            self.x -= self.velocity
+            if self.x <= 0:
+                self.x = 0
+        elif self.direction == 4:
+            self.y -= self.velocity
+            if self.y <= 0:
+                self.y = 0
+        for wall in thewall:
+            if self.x == wall.x and self.y == wall.y:
+                self.x = x1
+                self.y = y1
+        self.swallow()
+        self.set_coord(self.x, self.y)
+
+
+def process_events(events, packman):
+    for event in events:
+        if (event.type == QUIT) or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            sys.exit(0)
+        elif event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                packman.direction = 3
+            elif event.key == K_RIGHT:
+                packman.direction = 1
+            elif event.key == K_UP:
+                packman.direction = 4
+            elif event.key == K_DOWN:
+                packman.direction = 2
+            elif event.key == K_SPACE:
+                packman.direction = 0
+
+class Food(GameObject):
+    def __init__(self, x, y, tile_size, map_size):
+        GameObject.__init__(self, './resources/food.bmp', x, y, tile_size, map_size)
+
+class Map:
+        def __init__(self, w, h):
+                self.map = [[list()]*x for i in range(y)]
+
+        # Функция возвращает список обьектов в данной точке карты
+        def get(self, x, y):
+                return self.map[x][y]
+
+
+class Wall(GameObject):
+    def __init__(self, x, y, tile_size, map_size):
+        GameObject.__init__(self, './resources/wall.bmp', x, y, tile_size, map_size)
+
+"""
+def process_events(events):
+        for event in events:
+                if (event.type == QUIT) or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                        sys.exit(0)
+"""
+
+if __name__ == '__main__':
+        init_window()
+        tile_size = 32
+        map_size = 16
+        pacman = Pacman(1, 1, tile_size, map_size)
+        map_file = open('map.txt', 'r')
+        food_file = open('food.txt', 'r')
+        thewall = []
+        ghost = []
+        food = []
+        number_of_ghosts = 2
+        A = map_file.readlines()
+        B = food_file.readlines()
+        for i in range(len(A)-1):
+            A[i] = list(map(int, A[i].split()))
+            thewall.append(Wall(A[i][0], A[i][1], tile_size, map_size))
+        for i in range(number_of_ghosts):
+            ghost.append(Ghost(random.randint(1, 15), random.randint(1, 15), tile_size, map_size))
+        for i in range(len(B)):
+            B[i] = list(map(int, B[i].split()))
+            food.append(Food(B[i][0], B[i][1], tile_size, map_size))
+        background = pygame.image.load("./resources/background.bmp")
+        screen = pygame.display.get_surface()
+
+        while 1:
+                process_events(pygame.event.get(), pacman)
+                pygame.time.delay(100)
+                for i in range(len(ghost)):
+                    ghost[i].game_tick()
+                pacman.game_tick()
+                draw_background(screen, background)
+                for i in range(len(thewall)):
+                    thewall[i].draw(screen)
+                for i in range(len(food)):
+                   # if food[i].existance == 1:
+                    food[i].draw(screen)
+                for i in range(len(ghost)):
+                    ghost[i].draw(screen)
+                pacman.draw(screen)
+                pygame.display.update()
